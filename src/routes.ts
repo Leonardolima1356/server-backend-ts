6b17f613da18ac400cf1945979f418d17ab69a0f
Leonardo Lima <leonardolima1356@gmail.com>
2025-05-21 16:59:40 -0300

    Add files via upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import userController from "./controllers/user"
+import listController from "./controllers/list"
+
 const routes = Router();
 
 //rota de teste, hello word
@@ -66,5 +68,10 @@ routes.get("/users", (req: Request, res: Response) : any => userController.read(
 routes.put("/users/:id", (req: Request, res: Response) : any => userController.update(req,res))
 routes.delete("/users/:id", (req: Request, res: Response) : any => userController.delete(req,res))
 routes.post("/login", (req: Request, res: Response) : any => userController.login(req,res))
+
+routes.post("/lists", (req: Request, res: Response) : any => listController.create(req,res))
+routes.get("/lists", (req: Request, res: Response) : any => listController.read(req,res))
+routes.put("/lists/:id", (req: Request, res: Response) : any => listController.update(req,res))
+routes.delete("/lists/:id", (req: Request, res: Response) : any => listController.delete(req,res))
 
 export default routes
